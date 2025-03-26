1d8b22c6845b5b798b3340f0523c08216f1677b4
andirakha <andi17rakha@gmail.com>
2025-03-26 11:42:56 +0700

    Sidebar Improvement

--- Front-End/src/components/SideBar/SideBar.jsx
+++ Front-End/src/components/SideBar/SideBar.jsx
@@ -31,70 +31,68 @@ export default function SideBar({ ref = null}) {
             {sidebar && <div className={styles.overlay} onClick={toggleSidebar}></div>}
             <div className={`${styles.sidebarContainer} ${sidebar ? styles.expanded : ''}`}>
                 <div className={styles.wrapperMenu} >
-                    <button  onClick={toggleSidebar}>
-                    <img src={MenuIcon} alt="menu" />
+                    <button onClick={toggleSidebar}>
+                        <img src={MenuIcon} alt="menu" />
                     </button>
                     <h2 onClick={!sidebar ? toggleSidebar : null}>Navigation</h2>
                 </div>
-                <div className={styles.wrapperHome} style={{ backgroundColor: location.pathname === "/" ? "#5C6BC0" : "#E8EAF6", cursor: location.pathname === "/" ? "default" : "pointer" }} onClick={() => {if (!(location.pathname === "/")) {window.location.href = "/";}}}>
+                <div className={`${styles.wrapperHome} ${location.pathname === "/" ? styles.wrapperHomeSelected : styles.wrapperHomeUnselected}`} onClick={() => {if (!(location.pathname === "/")) {window.location.href = "/";}}}>
                     <a
                         href={location.pathname !== "/" ? "/" : undefined} 
                         onClick={(e) => { if (location.pathname === "/") e.preventDefault(); }}
                         style={{ cursor: location.pathname === "/" ? "default" : "pointer" }}
                     >
-                    <img src={HomeIcon} alt="" />
+                        <img src={HomeIcon} alt="" />
                     </a>
-                    <h3 style={{ color: location.pathname === "/" ? "#FFFFFF" : "#5c6bc0" }}>Home</h3>
+                    <h3>Home</h3>
                 </div>
-                <div className={styles.wrapper } style={{ backgroundColor: location.pathname === "/vcs" ? "#5C6BC0" : "#E8EAF6", cursor: location.pathname === "/vcs" ? "default" : "pointer"}} onClick={() => {if (!(location.pathname === "/vcs")) {window.location.href = "/vcs";}}}>
+                <div className={`${styles.wrapper} ${location.pathname === "/vcs" ? styles.wrapperSelected : styles.wrapperUnselected}`} onClick={() => {if (!(location.pathname === "/vcs")) {window.location.href = "/vcs";}}}>
                     <a
                         href={location.pathname !== "/vcs" ? "/vcs" : undefined} 
                         onClick={(e) => { if (location.pathname === "/vcs") e.preventDefault(); }}
                         style={{ cursor: location.pathname === "/vcs" ? "default" : "pointer" }}
                     >1</a>
-                    <h3 style={{ color: location.pathname === "/vcs" ? "#FFFFFF" : "#5c6bc0" }}>Vigenere Cipher Standard</h3>
+                    <h3>Vigenere Cipher Standard</h3>
                 </div>
-                <div className={styles.wrapper} style={{ backgroundColor: location.pathname === "/akvc" ? "#5C6BC0" : "#E8EAF6", cursor: location.pathname === "/akvc" ? "default" : "pointer"}} onClick={() => {if (!(location.pathname === "/akvc")) {window.location.href = "/akvc";}}}>
+                <div className={`${styles.wrapper} ${location.pathname === "/akvc" ? styles.wrapperSelected : styles.wrapperUnselected}`} onClick={() => {if (!(location.pathname === "/akvc")) {window.location.href = "/akvc";}}}>
                     <a
                         href={location.pathname !== "/akvc" ? "/akvc" : undefined} 
                         onClick={(e) => { if (location.pathname === "/akvc") e.preventDefault(); }}
                         style={{ cursor: location.pathname === "/akvc" ? "default" : "pointer" }}
                     >2</a>
-                    <h3 style={{ color: location.pathname === "/akvc" ? "#FFFFFF" : "#5c6bc0" }}>Auto-Key Vigenere Cipher</h3>
+                    <h3>Auto-Key Vigenere Cipher</h3>
                 </div>
-                <div className={styles.wrapper} style={{ backgroundColor: location.pathname === "/evc" ? "#5C6BC0" : "#E8EAF6", cursor: location.pathname === "/evc" ? "default" : "pointer"}} onClick={() => {if (!(location.pathname === "/evc")) {window.location.href = "/evc";}}}>
+                <div className={`${styles.wrapper} ${location.pathname === "/evc" ? styles.wrapperSelected : styles.wrapperUnselected}`} onClick={() => {if (!(location.pathname === "/evc")) {window.location.href = "/evc";}}}>
                     <a
                         href={location.pathname !== "/evc" ? "/evc" : undefined} 
                         onClick={(e) => { if (location.pathname === "/evc") e.preventDefault(); }}
                         style={{ cursor: location.pathname === "/evc" ? "default" : "pointer" }}
                     >3</a>
-                    <h3 style={{ color: location.pathname === "/evc" ? "#FFFFFF" : "#5c6bc0" }}>Extended Vigenere Cipher</h3>
+                    <h3>Extended Vigenere Cipher</h3>
                 </div>
-                <div className={styles.wrapper} style={{ backgroundColor: location.pathname === "/ac" ? "#5C6BC0" : "#E8EAF6", cursor: location.pathname === "/ac" ? "default" : "pointer"}} onClick={() => {if (!(location.pathname === "/ac")) {window.location.href = "/ac";}}}>
+                <div className={`${styles.wrapper} ${location.pathname === "/ac" ? styles.wrapperSelected : styles.wrapperUnselected}`} onClick={() => {if (!(location.pathname === "/ac")) {window.location.href = "/ac";}}}>
                     <a
                         href={location.pathname !== "/ac" ? "/ac" : undefined} 
                         onClick={(e) => { if (location.pathname === "/ac") e.preventDefault(); }}
                         style={{ cursor: location.pathname === "/ac" ? "default" : "pointer" }}
-                    >
-                    4</a>
-                    <h3 style={{ color: location.pathname === "/ac" ? "#FFFFFF" : "#5c6bc0" }}>Affine Cipher</h3>
+                    >4</a>
+                    <h3>Affine Cipher</h3>
                 </div>
-                <div className={styles.wrapper} style={{ backgroundColor: location.pathname === "/pc" ? "#5C6BC0" : "#E8EAF6", cursor: location.pathname === "/pc" ? "default" : "pointer"}} onClick={() => {if (!(location.pathname === "/pc")) {window.location.href = "/pc";}}}>
+                <div className={`${styles.wrapper} ${location.pathname === "/pc" ? styles.wrapperSelected : styles.wrapperUnselected}`} onClick={() => {if (!(location.pathname === "/pc")) {window.location.href = "/pc";}}}>
                     <a
                         href={location.pathname !== "/pc" ? "/pc" : undefined} 
                         onClick={(e) => { if (location.pathname === "/pc") e.preventDefault(); }}
                         style={{ cursor: location.pathname === "/pc" ? "default" : "pointer" }}
                     >5</a>
-                    <h3 style={{ color: location.pathname === "/pc" ? "#FFFFFF" : "#5c6bc0" }}>Playfair Cipher</h3>
+                    <h3>Playfair Cipher</h3>
                 </div>
-                <div className={styles.wrapper} style={{ backgroundColor: location.pathname === "/hc" ? "#5C6BC0" : "#E8EAF6", cursor: location.pathname === "/hc" ? "default" : "pointer"}} onClick={() => {if (!(location.pathname === "/hc")) {window.location.href = "/hc";}}}>
+                <div className={`${styles.wrapper} ${location.pathname === "/hc" ? styles.wrapperSelected : styles.wrapperUnselected}`} onClick={() => {if (!(location.pathname === "/hc")) {window.location.href = "/hc";}}}>
                     <a
                         href={location.pathname !== "/hc" ? "/hc" : undefined} 
                         onClick={(e) => { if (location.pathname === "/hc") e.preventDefault(); }}
                         style={{ cursor: location.pathname === "/hc" ? "default" : "pointer" }}
-                    >
-                    6</a>
-                    <h3 style={{ color: location.pathname === "/hc" ? "#FFFFFF" : "#5c6bc0" }}>Hill Cipher</h3>
+                    >6</a>
+                    <h3>Hill Cipher</h3>
                 </div>
             </div>
         </>
